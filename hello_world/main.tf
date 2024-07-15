71aeee03b100fd440a1cced448ee17e54716c6e0
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-07-15 12:58:07 +0200

    Update main.tf

--- hello_world/main.tf
+++ hello_world/main.tf
@@ -21,3 +21,15 @@ resource "azurerm_resource_group" "rg" {
     environment = "CP2"
   }
 }
+
+resource "azurerm_storage_account" "stAccount" {
+  name = "staccountcp2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group_rg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+
+  tag = {
+    environment = "CP2"
+  }
+}
