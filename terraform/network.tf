ad43a512f2e338db9256d63dcbdea41f7b76342c
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-07-15 15:33:36 +0200

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,9 +1,9 @@
 # Creación de una red (Virtual network)
 resource "azurerm_virtual_network" "myNet" {
-  name  =  "kubernetesnet"
-  address_space  =  ["10.0.0.0/16"]
-  location  =  azurerm_resource_group.rg.location
-  resource_group_name  =  azurerm_resource_group.rg.name
+  name                  =  "kubernetesnet"
+  address_space         =  ["10.0.0.0/16"]
+  location              =  azurerm_resource_group.rg.location
+  resource_group_name   =  azurerm_resource_group.rg.name
 
   tags = {
     enviroment = "CP2"
@@ -12,8 +12,40 @@ resource "azurerm_virtual_network" "myNet" {
 
 # Creación de subnet
 resource "azurerm_subnet" "mySubNet" {
-  name  =  "terraformsubnet"
-  resource_group_name  =  azurerm_resource_group.rg.name
+  name                  =  "terraformsubnet"
+  resource_group_name   =  azurerm_resource_group.rg.name
   virtual_network_name  =  azurerm_virtual_network.myNet.name
-  address_prefixes  =  ["10.0.0.0/24"]
+  address_prefixes      =  ["10.0.1.0/24"]
+}
+
+#Cracion de interfaz de red (NIC)
+resource "azurerm_network_interface" "myNIC" {
+  name                = "vmnic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myipconfiguration1"
+    subnet_id                     = azurerm_subnet.mySubNet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.10"
+    public_ip_address_id          =  azurerm_public_ip.myPublicIp1.id
+  }
+
+    tags  =  {
+      enviroment = "CP2"
+    }
+}
+
+# Creación IP Pública
+resource "azurerm_public_ip" "myPublicIp1"{
+  name  =  "vmip1"
+  location              =  azurerm_resource_group.rg.location
+  resource_group_name   =  azurerm_resource_group.rg.name
+  allocation_method     =  "Dynamic"
+  sku                   =  "Basic"
+
+    tags = {
+      enviroment = "CP2"
+    }
 }
