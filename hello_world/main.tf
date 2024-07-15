5d7891bdde299951013559c08879de3b41d63a1c
alegaz <alegaz@gmail.com>
2024-07-15 13:08:46 +0200

    Fix bug in main.tf

--- hello_world/main.tf
+++ hello_world/main.tf
@@ -23,13 +23,13 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "stAccount" {
-  name = "staccountcp2"
-  resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group_rg.location
-  account_tier = "Standard"
+  name                     = "staccountcp2"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  tag = {
+  tags = {
     environment = "CP2"
   }
 }
