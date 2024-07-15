2276ba064317ab9de0bed32ad4ebc67a0431dfdc
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-07-15 15:50:47 +0200

    Update vm.tf

---
+++ terraform/vm.tf
@@ -0,0 +1,36 @@
+# Creamos una máquina virtual
+resource "azurerm_linux_virtual_machine" "myVM1" {
+  name                = "cp1_vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = "adminuser"
+  network_interface_ids = [azurerm_network_interface.rg.id ]
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+  }
+
+  tags = {
+    enviroment  =  "CP2"
+  }
+
+}
