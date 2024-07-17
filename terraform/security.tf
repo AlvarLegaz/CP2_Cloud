76fa7fba61eb97b657c19ec3b88388d5e50366ab
alegaz <alegaz@gmail.com>
2024-07-17 19:54:03 +0200

    Update repo

--- terraform/security.tf
+++ terraform/security.tf
@@ -16,6 +16,18 @@ resource "azurerm_network_security_group" "mySecGroup" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "NGINX"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8090"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     environment = "CP2"
   }
