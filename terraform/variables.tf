68a1bcab3b6d95534f9d8e5a012ebd3a0e02f3d8
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-07-18 15:43:44 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,21 @@ variable "vm_size" {
   description = "Tamaño de la máquina virtual"
   default = "Standard_D1_v2" # 3.5 GB, 1 CPU
 }
+
+variable "node_count" {
+  type        = number
+  description = "The initial quantity of nodes for the node pool."
+  default     = 1
+}
+
+variable "msi_id" {
+  type        = string
+  description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
+  default     = null
+}
+
+variable "username" {
+  type        = string
+  description = "The admin username for the new cluster."
+  default     = "azureadmin"
+}
