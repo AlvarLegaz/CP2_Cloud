67d193332726478fc316d5c53c46e3fef956c4a7
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-07-18 13:01:01 +0200

    Actualizar main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,3 +33,21 @@ resource "azurerm_storage_account" "stAccount" {
     environment = "CP2"
   }
 }
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  service_principal {
+    client_id     = "YOUR_CLIENT_ID"
+    client_secret = "YOUR_CLIENT_SECRET"
+  }
+}
