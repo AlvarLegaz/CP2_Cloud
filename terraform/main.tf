b847d29d6202cf475a1353d3bd011e340456b52c
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-07-18 15:41:00 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,34 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.93.0"
+    }
+    
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
     }
   }
 
   required_version = ">= 1.1.0"
 }
 
+#Define el proveedor
 provider "azurerm" {
   features {}
 }
 
+#Define el resource group
 resource "azurerm_resource_group" "rg" {
   name     = "CP2_ResourceGroup"
   location = var.location
@@ -23,7 +40,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "stAccount" {
-  name                     = "staccountcp2"
+  name                     = "alegazstaccount"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -34,20 +51,46 @@ resource "azurerm_storage_account" "stAccount" {
   }
 }
 
-resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = "my-aks-cluster"
+#Define el cluster de kubernetes
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "alegazk8s"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "myaks"
+  dns_prefix          = "alegazk8snamespace"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
+  sku_tier = "Standard" # Nivel de precios como Standard, version free no disponible
+
+  identity {
+    type = "SystemAssigned"
   }
 
-  service_principal {
-    client_id     = "YOUR_CLIENT_ID"
-    client_secret = "YOUR_CLIENT_SECRET"
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.node_count
+  }
+  linux_profile {
+    admin_username = var.username
+
+    ssh_key {
+      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+}
+
+#Define el resource group
+resource "azurerm_container_registry" "acr" {
+  name                = "alegazCP2ContainerRegistry"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Basic"
+  admin_enabled       = true
+
+  tags = {
+    environment = "CP2"
   }
 }
